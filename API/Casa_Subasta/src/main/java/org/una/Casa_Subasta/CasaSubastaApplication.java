c66c596d7a3d8b5bcb160a13c53889ef8012b5c8
SergioVillr <70040516+SergioVillr@users.noreply.github.com>
2020-10-03 20:35:33 -0600

    Commit_inicial

---
+++ API/Casa_Subasta/src/main/java/org/una/Casa_Subasta/CasaSubastaApplication.java
@@ -0,0 +1,13 @@
+package org.una.Casa_Subasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaSubastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaSubastaApplication.class, args);
+	}
+
+}
---
+++ API/Casa_Subasta/src/test/java/org/una/Casa_Subasta/CasaSubastaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.Casa_Subasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaSubastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
